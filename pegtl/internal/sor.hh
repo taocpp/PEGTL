afdd68c2e3d2919a77c1fec6f9f801bfbf663d0d
Daniel Frey <d.frey@gmx.de>
2015-03-07 22:41:56 +0100

    Simplified

--- pegtl/internal/sor.hh
+++ pegtl/internal/sor.hh
@@ -4,7 +4,6 @@
 #ifndef PEGTL_INTERNAL_SOR_HH
 #define PEGTL_INTERNAL_SOR_HH
 
-#include "trivial.hh"
 #include "rule_disjunction.hh"
 
 #include "../analysis/generic.hh"
@@ -13,33 +12,15 @@ namespace pegtl
 {
    namespace internal
    {
-      template< typename ... Rules > struct sor;
-
-      template<>
-      struct sor<>
-            : trivial< false > {};
-
-      template< typename Rule >
-      struct sor< Rule >
+      template< typename ... Rules >
+      struct sor
       {
-         using analyze_t = typename Rule::analyze_t;
+         using analyze_t = analysis::generic< analysis::rule_type::SOR, Rules ... >;
 
          template< apply_mode A, template< typename ... > class Action, template< typename ... > class Control, typename Input, typename ... States >
          static bool match( Input & in, States && ... st )
          {
-            return Control< Rule >::template match< A, Action, Control >( in, st ... );
-         }
-      };
-
-      template< typename Rule, typename More, typename ... Rules >
-      struct sor< Rule, More, Rules ... >
-      {
-         using analyze_t = analysis::generic< analysis::rule_type::SOR, Rule, More, Rules ... >;
-
-         template< apply_mode A, template< typename ... > class Action, template< typename ... > class Control, typename Input, typename ... States >
-         static bool match( Input & in, States && ... st )
-         {
-            return rule_disjunction< Rule, More, Rules ... >::template match< A, Action, Control >( in, st ... );
+            return rule_disjunction< Rules ... >::template match< A, Action, Control >( in, st ... );
          }
       };
 
