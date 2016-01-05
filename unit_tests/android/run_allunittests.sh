1c25e87d477c8027fb6c524a8ed7b1db8ec92e5f
Kenneth Geisshirt <kg@realm.io>
2016-01-05 16:21:24 +0100

    Adding script to build a single unit test and run all unit tests

---
+++ unit_tests/android/run_allunittests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for name in $(find .. -name '*.cc' -exec grep -H "void unit_test" {} \; | cut -d/ -f2 | cut -d. -f1)
+do
+    echo "Building and running $name"
+    ./mk_unittest.sh $name
+    ./build_unittest.sh
+    ./run_unittest.sh
+    echo "Done $name"
+done
