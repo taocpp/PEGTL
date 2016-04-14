53ce964bfe175b15910d63fdd228e2fa07ef378b
Kuzma Shapran <Kuzma.Shapran@umajin.com>
2016-04-14 16:43:21 +1200

    Update unit tests

--- unit_tests/ascii_eolf.cc
+++ unit_tests/ascii_eolf.cc
@@ -12,15 +12,22 @@ namespace pegtl
       verify_rule< eolf >( __LINE__, __FILE__,  "", result_type::SUCCESS, 0 );
 
       for ( char i = 1; i < 127; ++i ) {
-         verify_char< eolf >( __LINE__, __FILE__, i, ( i == '\n' ) ? result_type::SUCCESS : result_type::LOCAL_FAILURE );
+         verify_char< eolf >( __LINE__, __FILE__, i, (( i == '\n' ) || ( i == '\r' )) ? result_type::SUCCESS : result_type::LOCAL_FAILURE );
       }
       verify_rule< eolf >( __LINE__, __FILE__,  "\r\n", result_type::SUCCESS, 0 );
       verify_rule< eolf >( __LINE__, __FILE__,  "\n\r", result_type::SUCCESS, 1 );
       verify_rule< eolf >( __LINE__, __FILE__,  "\na", result_type::SUCCESS, 1 );
-      verify_rule< eolf >( __LINE__, __FILE__,  "\ra", result_type::LOCAL_FAILURE, 2 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\ra", result_type::SUCCESS, 1 );
       verify_rule< eolf >( __LINE__, __FILE__,  "\r\na", result_type::SUCCESS, 1 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\n\ra", result_type::SUCCESS, 2 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\r\r\r", result_type::SUCCESS, 2 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\r\r\n", result_type::SUCCESS, 2 );
       verify_rule< eolf >( __LINE__, __FILE__,  "\r\n\r", result_type::SUCCESS, 1 );
       verify_rule< eolf >( __LINE__, __FILE__,  "\r\n\n", result_type::SUCCESS, 1 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\n\r\r", result_type::SUCCESS, 2 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\n\r\n", result_type::SUCCESS, 2 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\n\n\r", result_type::SUCCESS, 2 );
+      verify_rule< eolf >( __LINE__, __FILE__,  "\n\n\n", result_type::SUCCESS, 2 );
    }
 
 } // pegtl
