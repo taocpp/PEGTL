8d42727772ca2264f0a8fc83badaef0eb6cdf782
Colin Hirsch <colinh-github-06134@icemx.net>
2016-11-28 09:48:34 +0100

    Add actions.

--- examples/csv2.cc
+++ examples/csv2.cc
@@ -1,6 +1,6 @@
 // Copyright (c) 2016 Dr. Colin Hirsch and Daniel Frey
 // Please see LICENSE for license or visit https://github.com/ColinH/PEGTL/
-
+#include <utility>
 #include <iostream>
 
 #include <pegtl.hh>
@@ -26,13 +26,138 @@ namespace csv2
    template< unsigned N > struct line : pegtl::seq< value, pegtl::rep< N - 1, pegtl::one< ',' >, value >, pegtl::eol > {};
    template< unsigned N > struct file : pegtl::until< pegtl::eof, line< N > > { static_assert( N, "N must be positive" ); };
 
+   // Meta-programming helper:
+
+   template< unsigned N, typename T > struct tuple_help;
+
+   template< unsigned N, typename ... S > struct tuple_help< N, std::tuple< S ... > >
+   {
+      using tuple_t = typename tuple_help< N - 1, std::tuple< std::string, S ... > >::tuple_t;
+   };
+
+   template< typename ... S > struct tuple_help< 0, std::tuple< S ... > >
+   {
+      using tuple_t = std::tuple< S ... >;
+   };
+
+   // Ad-hoc helper to initialise a tuple from a vector:
+
+   template< unsigned I > struct tuple_init
+   {
+      template< typename ... S >
+      static void init( std::tuple< S ... > & t, std::vector< std::string > & v )
+      {
+         std::get< I >( t ) = std::move( v[ I ] );
+         tuple_init< I - 1 >::init( t, v );
+      }
+   };
+
+   template<> struct tuple_init< 0 >
+   {
+      template< typename ... S >
+      static void init( std::tuple< S ... > & t, std::vector< std::string > & v )
+      {
+         std::get< 0 >( t ) = std::move( v[ 0 ] );
+      }
+   };
+
+   // Data structure to store the result of a parsing run:
+
+   template< unsigned N > struct result_data
+   {
+      using tuple_t = typename tuple_help< N, std::tuple<> >::tuple_t;
+
+      std::vector< std::string > temp;
+      std::vector< tuple_t > result;
+   };
+
+   // Action class to fill in the above data structure:
+
+   template< typename Rule > struct action : pegtl::nothing< Rule > {};
+
+   template<> struct action< plain_value >
+   {
+      template< unsigned N >
+      static void apply( const pegtl::action_input & in, result_data< N > & data )
+      {
+         data.temp.push_back( in.string() );
+      }
+   };
+
+   template<> struct action< string_without< '"' > >
+         : action< plain_value > {};
+
+   template< unsigned N > struct action< line< N > >
+   {
+      using tuple_t = typename tuple_help< N, std::tuple<> >::tuple_t;
+
+      static void apply( const pegtl::action_input & in, result_data< N > & data )
+      {
+         if ( data.temp.size() != N ) {
+            throw pegtl::parse_error( "column count mismatch", in );
+         }
+         tuple_t temp;
+         tuple_init< N - 1 >::init( temp, data.temp );
+         data.result.emplace_back( std::move( temp ) );
+         data.temp.clear();
+      }
+   };
+
+   // Another helper to print tuples of arbitrary sizes:
+
+   void print_string( const std::string & s )
+   {
+      // Needs more elaborate escaping in practice...
+
+      if ( s.find( ',' ) != std::string::npos ) {
+         std::cout << '"' << s << '"';
+      }
+      else {
+         std::cout << s;
+      }
+   }
+
+   template< unsigned I > struct print_help
+   {
+      template< typename ... S >
+      static void print( const std::tuple< S ... > & t )
+      {
+         print_help< I - 1 >::print( t );
+         std::cout << ',';
+         print_string( std::get< I >( t ) );
+      }
+   };
+
+   template<> struct print_help< 0 >
+   {
+      template< typename ... S >
+      static void print( const std::tuple< S ... > & t )
+      {
+         print_string( std::get< 0 >( t ) );
+      }
+   };
+
+   template< typename ... S >
+   void print_tuple( const std::tuple< S ... > & t )
+   {
+      constexpr unsigned size = sizeof...( S );
+      static_assert( size, "empty tuple doesn't work here" );
+      print_help< size - 1 >::print( t );
+      std::cout << std::endl;
+   }
+
 } // csv2
 
 int main( int argc, char ** argv )
 {
    for ( int i = 1; i < argc; ++i ) {
       pegtl::file_parser fp( argv[ i ] );
-      std::cout << argv[ i ] << " " << fp.parse< csv2::file< 3 > >() << std::endl;
+      constexpr unsigned number_of_columns = 3;
+      csv2::result_data< number_of_columns > data;
+      fp.parse< pegtl::must< csv2::file< number_of_columns > >, csv2::action >( data );
+      for ( const auto & line : data.result ) {
+         csv2::print_tuple( line );
+      }
    }
    return 0;
 }
