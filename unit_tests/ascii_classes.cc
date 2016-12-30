54c100976f370e1df194b03a948231ad291e1cb5
Daniel Frey <d.frey@gmx.de>
2016-12-30 14:58:51 +0100

    Fix some warnings

--- unit_tests/ascii_classes.cc
+++ unit_tests/ascii_classes.cc
@@ -58,50 +58,52 @@ namespace pegtl
       verify_rule< ranges< 'a', 'z', '4' > >( __LINE__, __FILE__, "", result_type::LOCAL_FAILURE, 0 );
 
       for ( int i = -100; i < 200; ++i ) {
-         const bool is_blank = ( i == ' ' ) || ( i == '\t' );
-         const bool is_digit = ( '0' <= i ) && ( i <= '9' );
-         const bool is_lower = ( 'a' <= i ) && ( i <= 'z' );
-         const bool is_print = ( ( ' ' <= i ) && ( i <= 126 ) );
+         const char c = char( i );
+
+         const bool is_blank = ( c == ' ' ) || ( c == '\t' );
+         const bool is_digit = ( '0' <= c ) && ( c <= '9' );
+         const bool is_lower = ( 'a' <= c ) && ( c <= 'z' );
+         const bool is_print = ( ( ' ' <= c ) && ( c <= 126 ) );
          const bool is_seven = ( ( i >= 0 ) && ( i <= 127 ) );
-         const bool is_space = ( i == '\n' ) || ( i == '\r' ) || ( i == '\v' ) || ( i == '\f' );
-         const bool is_upper = ( 'A' <= i ) && ( i <= 'Z' );
-         const bool is_xalpha = ( ( 'a' <= i ) && ( i <= 'f' ) ) || ( ( 'A' <= i ) && ( i <= 'F' ) );
+         const bool is_space = ( c == '\n' ) || ( c == '\r' ) || ( c == '\v' ) || ( c == '\f' );
+         const bool is_upper = ( 'A' <= c ) && ( c <= 'Z' );
+         const bool is_xalpha = ( ( 'a' <= c ) && ( c <= 'f' ) ) || ( ( 'A' <= c ) && ( c <= 'F' ) );
 
-         const bool is_newline = ( i == '\n' );
+         const bool is_newline = ( c == '\n' );
 
-         const bool is_ident_first = ( i == '_' ) || is_lower || is_upper;
+         const bool is_ident_first = ( c == '_' ) || is_lower || is_upper;
          const bool is_ident_other = is_ident_first || is_digit;
 
-         verify_char< alnum >( __LINE__, __FILE__, i, is_lower || is_upper || is_digit );
-         verify_char< alpha >( __LINE__, __FILE__, i, is_lower || is_upper );
-         verify_char< any >( __LINE__, __FILE__, i, true );
-         verify_char< blank >( __LINE__, __FILE__, i, is_blank );
-         verify_char< digit >( __LINE__, __FILE__, i, is_digit );
-         verify_char< eol >( __LINE__, __FILE__, i, is_newline );
-         verify_char< identifier_first >( __LINE__, __FILE__, i, is_ident_first );
-         verify_char< identifier_other >( __LINE__, __FILE__, i, is_ident_other );
-         verify_char< lower >( __LINE__, __FILE__, i, is_lower );
-         verify_char< nul >( __LINE__, __FILE__, i, i == 0 );
-         verify_char< print >( __LINE__, __FILE__, i, is_print );
-         verify_char< seven >( __LINE__, __FILE__, i, is_seven );
-         verify_char< space >( __LINE__, __FILE__, i, is_blank || is_space );
-         verify_char< upper >( __LINE__, __FILE__, i, is_upper );
-         verify_char< xdigit >( __LINE__, __FILE__, i, is_digit || is_xalpha );
+         verify_char< alnum >( __LINE__, __FILE__, c, is_lower || is_upper || is_digit );
+         verify_char< alpha >( __LINE__, __FILE__, c, is_lower || is_upper );
+         verify_char< any >( __LINE__, __FILE__, c, true );
+         verify_char< blank >( __LINE__, __FILE__, c, is_blank );
+         verify_char< digit >( __LINE__, __FILE__, c, is_digit );
+         verify_char< eol >( __LINE__, __FILE__, c, is_newline );
+         verify_char< identifier_first >( __LINE__, __FILE__, c, is_ident_first );
+         verify_char< identifier_other >( __LINE__, __FILE__, c, is_ident_other );
+         verify_char< lower >( __LINE__, __FILE__, c, is_lower );
+         verify_char< nul >( __LINE__, __FILE__, c, c == 0 );
+         verify_char< print >( __LINE__, __FILE__, c, is_print );
+         verify_char< seven >( __LINE__, __FILE__, c, is_seven );
+         verify_char< space >( __LINE__, __FILE__, c, is_blank || is_space );
+         verify_char< upper >( __LINE__, __FILE__, c, is_upper );
+         verify_char< xdigit >( __LINE__, __FILE__, c, is_digit || is_xalpha );
 
-         const bool is_one = ( i == '#' ) || ( i == 'a' ) || ( i == ' ' );
-         const bool is_range = ( 20 <= i ) && ( i <= 120 );
-         const bool is_ranges = is_range || ( i == 3 );
+         const bool is_one = ( c == '#' ) || ( c == 'a' ) || ( c == ' ' );
+         const bool is_range = ( 20 <= c ) && ( c <= 120 );
+         const bool is_ranges = is_range || ( c == 3 );
 
-         verify_char< not_one< 'P' > >( __LINE__, __FILE__, i, i != 'P' );
-         verify_char< not_one< 'a', '#', ' ' > >( __LINE__, __FILE__, i, ! is_one );
-         verify_char< not_range< 20, 120 > >( __LINE__, __FILE__, i, ! is_range );
-         verify_char< one< 'T' > >( __LINE__, __FILE__, i, i == 'T' );
-         verify_char< one< 'a', '#', ' ' > >( __LINE__, __FILE__, i, is_one );
-         verify_char< range< 20, 120 > >( __LINE__, __FILE__, i, is_range );
-         verify_char< ranges< 20, 120 > >( __LINE__, __FILE__, i, is_range );
-         verify_char< ranges< 20, 120, 3 > >( __LINE__, __FILE__, i, is_ranges );
+         verify_char< not_one< 'P' > >( __LINE__, __FILE__, c, c != 'P' );
+         verify_char< not_one< 'a', '#', ' ' > >( __LINE__, __FILE__, c, ! is_one );
+         verify_char< not_range< 20, 120 > >( __LINE__, __FILE__, c, ! is_range );
+         verify_char< one< 'T' > >( __LINE__, __FILE__, c, c == 'T' );
+         verify_char< one< 'a', '#', ' ' > >( __LINE__, __FILE__, c, is_one );
+         verify_char< range< 20, 120 > >( __LINE__, __FILE__, c, is_range );
+         verify_char< ranges< 20, 120 > >( __LINE__, __FILE__, c, is_range );
+         verify_char< ranges< 20, 120, 3 > >( __LINE__, __FILE__, c, is_ranges );
 
-         verify_char< eolf >( __LINE__, __FILE__, i, is_newline );
+         verify_char< eolf >( __LINE__, __FILE__, c, is_newline );
       }
    }
 
