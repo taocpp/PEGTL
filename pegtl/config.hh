bf8b5ee88bbf481211df7c3ec8aeecd4f50c8485
Daniel Frey <d.frey@gmx.de>
2017-01-16 22:47:00 +0100

    New

---
+++ pegtl/config.hh
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/ColinH/PEGTL/
+
+#ifndef PEGTL_CONFIG_HH
+#define PEGTL_CONFIG_HH
+
+#include "version.hh"
+
+#ifndef PEGTL_NAMESPACE
+#define PEGTL_NAMESPACE pegtl
+#endif
+
+#endif
---
+++ pegtl/version.hh
@@ -0,0 +1,13 @@
+// Copyright (c) 2017 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/ColinH/PEGTL/
+
+#ifndef PEGTL_VERSION_HH
+#define PEGTL_VERSION_HH
+
+#define PEGTL_VERSION_MAJOR 2
+#define PEGTL_VERSION_MINOR 0
+#define PEGTL_VERSION_PATCH 0
+
+#define PEGTL_VERSION "2.0.0"
+
+#endif
