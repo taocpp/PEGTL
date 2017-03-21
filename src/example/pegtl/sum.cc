bb93ae6cd84378ebe0743e98e2e387bd696b1325
Daniel Frey <d.frey@gmx.de>
2017-03-21 19:40:29 +0100

    Add header for strtod

--- src/example/pegtl/sum.cc
+++ src/example/pegtl/sum.cc
@@ -1,6 +1,7 @@
 // Copyright (c) 2014-2017 Dr. Colin Hirsch and Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
 
+#include <cstdlib>
 #include <string>
 #include <iostream>
 
