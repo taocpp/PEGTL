a0991aea76c201dd1e7c7fae88ebc1f7ea726425
Daniel Frey <d.frey@gmx.de>
2017-04-13 22:07:33 +0200

    New

---
+++ include/tao/pegtl/position_tracking.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2017 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
+
+#ifndef TAOCPP_PEGTL_INCLUDE_POSITION_TRACKING_HPP
+#define TAOCPP_PEGTL_INCLUDE_POSITION_TRACKING_HPP
+
+#include "config.hpp"
+
+namespace tao
+{
+   namespace TAOCPP_PEGTL_NAMESPACE
+   {
+      enum class position_tracking
+      {
+         IMMEDIATE,
+         LAZY
+      };
+
+   }  // namespace TAOCPP_PEGTL_NAMESPACE
+
+}  // namespace tao
+
+#endif
