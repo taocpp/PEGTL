cdc9b6e4b0dd8cc78d947469f66067dd8adc8623
Colin Hirsch <colinh-github-06134@icemx.net>
2017-04-13 11:09:46 +0200

    Add analysis example.

---
+++ src/example/pegtl/analyze.cpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2017 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
+
+#include <tao/pegtl.hpp>
+#include <tao/pegtl/analyze.hpp>
+
+using namespace tao::TAOCPP_PEGTL_NAMESPACE;
+
+struct bar;
+
+struct foo
+   : sor< digit, bar > {};
+
+struct bar
+   : plus< foo > {};
+
+int main( int, char** )
+{
+   if( analyze< foo >() ) {
+      std::cout << "there are problems" << std::endl;
+      return 1;
+   }
+   return 0;
+}
