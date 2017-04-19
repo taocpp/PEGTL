432a3050f060b735c3aa49b6d27f3db1d19dca3b
Daniel Frey <d.frey@gmx.de>
2017-04-19 19:19:56 +0200

    Improve error reporting

--- src/example/pegtl/recover.cpp
+++ src/example/pegtl/recover.cpp
@@ -21,13 +21,16 @@ using namespace tao::TAOCPP_PEGTL_NAMESPACE;
 
 // clang-format off
 
-template< typename R, typename T > struct skipping : until< T > {};
-template< typename R, typename T > struct recoverable : sor< seq< R, T >, skipping< R, T > > {};
+template< typename T >
+struct skipping : until< T > {};
+
+template< typename R, typename T >
+struct recoverable : sor< try_catch< must< R, T > >, skipping< T > > {};
 
 struct sum;
-struct value : sor< plus< digit >, seq< one< '(' >, sum, one< ')' > > > {};
-struct prod : list< value, one< '*', '/' > > {};
-struct sum : list< prod, one< '+', '-' > > {};
+struct value : sor< plus< digit >, if_must< one< '(' >, sum, one< ')' > > > {};
+struct prod : list< must< value >, one< '*', '/' > > {};
+struct sum : list_must< prod, one< '+', '-' > > {};
 
 struct term : sor< one< ';' >, eof > {};
 struct expr : recoverable< sum, term > {};
@@ -41,14 +44,14 @@ struct my_action
 {
 };
 
-template< typename R, typename T >
-struct my_action< skipping< R, T > >
+template< typename T >
+struct my_action< skipping< T > >
 {
    template< typename Input >
    static void apply( const Input& in, bool& error )
    {
       if( !error ) {
-         std::cout << in.position() << ": Input \"" << in.string() << "\" does not match " << internal::demangle< R >() << " - skipping" << std::endl;
+         std::cout << in.position() << ": Invalid expression \"" << in.string() << "\"" << std::endl;
       }
       error = true;
    }
@@ -84,12 +87,24 @@ struct my_action< expr >
    }
 };
 
+template< typename Rule >
+struct my_control
+   : normal< Rule >
+{
+   template< typename Input, typename... States >
+   static void raise( const Input& in, States&&... )
+   {
+      std::cout << in.position() << ": Parse error matching " << internal::demangle< Rule >() << std::endl;
+      throw parse_error( "parse error matching " + internal::demangle< Rule >(), in );
+   }
+};
+
 int main( int argc, char** argv )
 {
    for( int i = 1; i < argc; ++i ) {
       argv_input<> in( argv, i );
       bool error = false;
-      parse< my_grammar, my_action >( in, error );
+      parse< my_grammar, my_action, my_control >( in, error );
    }
    return 0;
 }
