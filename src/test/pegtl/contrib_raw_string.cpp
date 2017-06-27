bc72f91210d86b93810b011f8748fde541c12c25
Daniel Frey <d.frey@gmx.de>
2017-06-27 17:53:27 +0200

    Fix test to catch inconsistencies

--- src/test/pegtl/contrib_raw_string.cpp
+++ src/test/pegtl/contrib_raw_string.cpp
@@ -15,13 +15,13 @@ namespace tao
       using qstring = raw_string< '[', '=', ']', alpha, digit >;
 
       template< typename Rule >
-      struct rqaction
+      struct raction
          : nothing< Rule >
       {
       };
 
       template<>
-      struct rqaction< rstring::content >
+      struct raction< rstring::content >
       {
          template< typename Input, typename... States >
          static void apply( const Input& in, const States&... /*unused*/ )
@@ -30,8 +30,14 @@ namespace tao
          }
       };
 
+      template< typename Rule >
+      struct qaction
+         : nothing< Rule >
+      {
+      };
+
       template<>
-      struct rqaction< qstring::content >
+      struct qaction< qstring::content >
       {
          template< typename Input, typename... States >
          static void apply( const Input& in, const States&... /*unused*/ )
@@ -50,18 +56,18 @@ namespace tao
       {
       };
 
-      template< typename Rule, unsigned M, unsigned N >
+      template< typename Rule, template< typename Rule > class Action, unsigned M, unsigned N >
       void verify_data( const std::size_t line, const char* file, const char ( &m )[ M ], const char ( &n )[ N ] )
       {
          content.clear();
          memory_input<> in( m, m + M - 1, file, 0, line, 0 );
-         const auto r = parse< Rule, rqaction >( in );
+         const auto r = parse< Rule, Action >( in );
          if( ( !r ) || ( content != std::string( n, N - 1 ) ) ) {
             TAOCPP_PEGTL_TEST_FAILED( "input data [ '" << m << "' ] expected success with [ '" << n << "' ] but got [ '" << content << "' ] result [ " << r << " ]" );
          }
          content.clear();
          memory_input< tracking_mode::LAZY > in2( m, m + M - 1, file, 0, line, 0 );
-         const auto r2 = parse< Rule, rqaction >( in2 );
+         const auto r2 = parse< Rule, Action >( in2 );
          if( ( !r2 ) || ( content != std::string( n, N - 1 ) ) ) {
             TAOCPP_PEGTL_TEST_FAILED( "input data [ '" << m << "' ] with tracking_mode::LAZY expected success with [ '" << n << "' ] but got [ '" << content << "' ] result [ " << r2 << " ]" );
          }
@@ -69,19 +75,19 @@ namespace tao
 
       void unit_test()
       {
-         verify_data< rgrammar >( __LINE__, __FILE__, "[[]]", "" );
-         verify_data< rgrammar >( __LINE__, __FILE__, "[[foo]]", "foo" );
-         verify_data< rgrammar >( __LINE__, __FILE__, "[===[foo]===]", "foo" );
-         verify_data< rgrammar >( __LINE__, __FILE__, "[===[\nfoo]===]", "foo" );
-         verify_data< rgrammar >( __LINE__, __FILE__, "[===[\r\nfoo]===]", "foo" );
-         verify_data< rgrammar >( __LINE__, __FILE__, "[===[\0\0\0]===]", "\0\0\0" );
+         verify_data< rgrammar, raction >( __LINE__, __FILE__, "[[]]", "" );
+         verify_data< rgrammar, raction >( __LINE__, __FILE__, "[[foo]]", "foo" );
+         verify_data< rgrammar, raction >( __LINE__, __FILE__, "[===[foo]===]", "foo" );
+         verify_data< rgrammar, raction >( __LINE__, __FILE__, "[===[\nfoo]===]", "foo" );
+         verify_data< rgrammar, raction >( __LINE__, __FILE__, "[===[\r\nfoo]===]", "foo" );
+         verify_data< rgrammar, raction >( __LINE__, __FILE__, "[===[\0\0\0]===]", "\0\0\0" );
 
-         verify_data< qgrammar >( __LINE__, __FILE__, "[[]]", "" );
-         verify_data< qgrammar >( __LINE__, __FILE__, "[[a1]]", "a1" );
-         verify_data< qgrammar >( __LINE__, __FILE__, "[===[a1]===]", "a1" );
-         verify_data< qgrammar >( __LINE__, __FILE__, "[===[\na1]===]", "a1" );
-         verify_data< qgrammar >( __LINE__, __FILE__, "[===[\r\na1]===]", "a1" );
-         verify_data< qgrammar >( __LINE__, __FILE__, "[===[a0a1a2a3]===]", "a0a1a2a3" );
+         verify_data< qgrammar, qaction >( __LINE__, __FILE__, "[[]]", "" );
+         verify_data< qgrammar, qaction >( __LINE__, __FILE__, "[[a1]]", "a1" );
+         verify_data< qgrammar, qaction >( __LINE__, __FILE__, "[===[a1]===]", "a1" );
+         verify_data< qgrammar, qaction >( __LINE__, __FILE__, "[===[\na1]===]", "a1" );
+         verify_data< qgrammar, qaction >( __LINE__, __FILE__, "[===[\r\na1]===]", "a1" );
+         verify_data< qgrammar, qaction >( __LINE__, __FILE__, "[===[a0a1a2a3]===]", "a0a1a2a3" );
 
          verify_fail< rgrammar >( __LINE__, __FILE__, "" );
          verify_fail< rgrammar >( __LINE__, __FILE__, "[" );
