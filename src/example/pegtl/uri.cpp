acc0e6e0aef418f8139f8a90e0cc8fce891c846a
Daniel Frey <d.frey@gmx.de>
2017-12-13 09:58:33 +0100

    New, example for #79

---
+++ src/example/pegtl/uri.cpp
@@ -0,0 +1,88 @@
+#include <tao/pegtl.hpp>
+#include <tao/pegtl/contrib/uri.hpp>
+
+#include <iostream>
+
+namespace pegtl = tao::TAOCPP_PEGTL_NAMESPACE;
+
+struct URI
+{
+   std::string scheme;
+   std::string authority;
+   std::string userinfo;
+   std::string host;
+   std::string port;
+   std::string path;
+   std::string query;
+   std::string fragment;
+
+   explicit URI( const std::string& uri );
+};
+
+namespace uri
+{
+   template< std::string URI::*Field >
+   struct bind
+   {
+      template< typename Input >
+      static void apply( const Input& in, URI& uri )
+      {
+         uri.*Field = in.string();
+      }
+   };
+
+   // clang-format off
+   template< typename Rule > struct action : tao::pegtl::nothing< Rule > {};
+
+   template<> struct action< pegtl::uri::scheme > : bind< &URI::scheme > {};
+   template<> struct action< pegtl::uri::authority > : bind< &URI::authority > {};
+   // userinfo: see below
+   template<> struct action< pegtl::uri::host > : bind< &URI::host > {};
+   template<> struct action< pegtl::uri::port > : bind< &URI::port > {};
+   template<> struct action< pegtl::uri::path_noscheme > : bind< &URI::path > {};
+   template<> struct action< pegtl::uri::path_rootless > : bind< &URI::path > {};
+   template<> struct action< pegtl::uri::path_absolute > : bind< &URI::path > {};
+   template<> struct action< pegtl::uri::path_abempty > : bind< &URI::path > {};
+   template<> struct action< pegtl::uri::query > : bind< &URI::query > {};
+   template<> struct action< pegtl::uri::fragment > : bind< &URI::fragment > {};
+   // clang-format on
+
+   template<>
+   struct action< pegtl::uri::opt_userinfo >
+   {
+      template< typename Input >
+      static void apply( const Input& in, URI& uri )
+      {
+         if( !in.empty() ) {
+            uri.userinfo = std::string( in.begin(), in.size() - 1 );
+         }
+      }
+   };
+
+   struct grammar : pegtl::must< pegtl::uri::URI >
+   {
+   };
+}
+
+URI::URI( const std::string& uri )
+{
+   pegtl::memory_input<> input( uri.data(), uri.size(), "uri" );
+   pegtl::parse< uri::grammar, uri::action >( input, *this );
+}
+
+int main( int argc, char** argv )
+{
+   for( int i = 1; i < argc; ++i ) {
+      std::cout << "Parsing " << argv[ i ] << std::endl;
+      const URI uri( argv[ i ] );
+      std::cout << "URI.scheme: " << uri.scheme << std::endl;
+      std::cout << "URI.authority: " << uri.authority << std::endl;
+      std::cout << "URI.userinfo: " << uri.userinfo << std::endl;
+      std::cout << "URI.host: " << uri.host << std::endl;
+      std::cout << "URI.port: " << uri.port << std::endl;
+      std::cout << "URI.path: " << uri.path << std::endl;
+      std::cout << "URI.query: " << uri.query << std::endl;
+      std::cout << "URI.fragment: " << uri.fragment << std::endl;
+   }
+   return 0;
+}
