d36f2cc07114bf76c20a696ce09749cf71644e95
Uilian Ries <uilianries@gmail.com>
2018-06-06 20:30:12 -0300

    #111 Create Conan package
    - Create PEGTL Conan package
    - Update test_package to check find_package (cmake)
    - Update CMake file to use Conan if present
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from conans import ConanFile, CMake
-import os
 
 class PEGTLConan(ConanFile):
     name = "pegtl"
+    version = "2.5.2"
     description = "C++11 header-only parser combinator library for creating PEG parsers"
     homepage = "https://github.com/taocpp/PEGTL"
     url = homepage
     license = "MIT"
     author = "taocpp@icemx.net"
-    exports_sources = "include*", "LICENSE", "CMakeLists.txt"
+    exports = "LICENSE"
+    exports_sources = "include/*", "CMakeLists.txt"
+    generators = "cmake"
+    no_copy_source = True
+
+    def build(self):
+        pass
 
     def package(self):
         cmake = CMake(self)
@@ -16,6 +24,7 @@ class PEGTLConan(ConanFile):
         cmake.definitions["PEGTL_BUILD_TESTS"] = "OFF"
         cmake.definitions["PEGTL_BUILD_EXAMPLES"] = "OFF"
         cmake.definitions["PEGTL_INSTALL_DOC_DIR"] = "licenses"
+        cmake.definitions["PEGTL_INSTALL_CMAKE_DIR"] = self.package_folder
 
         cmake.configure()
         cmake.install()
