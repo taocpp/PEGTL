59cb86d8085eb546649838dafbf2122c7a55a1ca
Daniel Frey <d.frey@gmx.de>
2018-09-07 18:24:49 +0200

    Improve coverage

--- src/test/pegtl/contrib_parse_tree.cpp
+++ src/test/pegtl/contrib_parse_tree.cpp
@@ -19,43 +19,33 @@ namespace tao
 
       template< typename Rule >
       struct selector
-         : parse_tree::selector< Rule, parse_tree::apply_remove_content::to< A, B, C, D > >
+         : parse_tree::selector< Rule, parse_tree::apply_store_content::to< A, B, C, D > >
       {
       };
-
-      void print_node( const parse_tree::node& n, const std::string& s = "" )
-      {
-         // detect the root node:
-         if( n.is_root() ) {
-            std::cout << "ROOT" << std::endl;
-         }
-         else {
-            if( n.has_content() ) {
-               std::cout << s << n.name() << " \"" << n.content() << "\" at " << n.begin() << " to " << n.end() << std::endl;
-            }
-            else {
-               std::cout << s << n.name() << " at " << n.begin() << std::endl;
-            }
-         }
-         // print all child nodes
-         if( !n.children.empty() ) {
-            const auto s2 = s + "  ";
-            for( auto& up : n.children ) {
-               print_node( *up, s2 );
-            }
-         }
-      }
 
       void unit_test()
       {
          memory_input<> in( "ac", "input" );
          const auto r = parse_tree::parse< D, selector >( in );
+         TAO_PEGTL_TEST_ASSERT( r->is_root() );
+         TAO_PEGTL_TEST_ASSERT( !r->has_content() );
          TAO_PEGTL_TEST_ASSERT( r->children.size() == 1 );
          const auto* d = r->children.begin()->get();
+         TAO_PEGTL_TEST_ASSERT( !d->is_root() );
          TAO_PEGTL_TEST_ASSERT( d->id == &typeid( D ) );
+         TAO_PEGTL_TEST_ASSERT( d->is< D >() );
+         TAO_PEGTL_TEST_ASSERT( d->name() == "tao::pegtl::D" );
+         TAO_PEGTL_TEST_ASSERT( d->has_content() );
+         TAO_PEGTL_TEST_ASSERT( d->begin().byte == 0 );
+         TAO_PEGTL_TEST_ASSERT( d->end().byte == 2 );
+         TAO_PEGTL_TEST_ASSERT( d->content() == "ac" );
          TAO_PEGTL_TEST_ASSERT( d->children.size() == 2 );
-         TAO_PEGTL_TEST_ASSERT( d->children.front()->id == &typeid( A ) );
-         TAO_PEGTL_TEST_ASSERT( d->children.back()->id == &typeid( C ) );
+         TAO_PEGTL_TEST_ASSERT( d->children.front()->is< A >() );
+         TAO_PEGTL_TEST_ASSERT( d->children.back()->is< C >() );
+
+         memory_input<> in2( "x", "input" );
+         const auto r2 = parse_tree::parse< D, selector >( in2 );
+         TAO_PEGTL_TEST_ASSERT( !r2 );
       }
 
    }  // namespace TAO_PEGTL_NAMESPACE
