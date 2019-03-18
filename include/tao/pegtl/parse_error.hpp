a01948218dd3aed6e5b88d7adaabb47dd0ac304d
Daniel Frey <d.frey@gmx.de>
2019-03-18 18:55:45 +0100

    Allow moving a position

--- include/tao/pegtl/parse_error.hpp
+++ include/tao/pegtl/parse_error.hpp
@@ -35,6 +35,13 @@ namespace tao
          {
          }
 
+         parse_error( const std::string& msg, position&& pos )
+            : std::runtime_error( to_string( pos ) + ": " + msg ),
+              positions()
+         {
+            positions.emplace_back( std::move( pos ) );
+         }
+
          std::vector< position > positions;
       };
 
