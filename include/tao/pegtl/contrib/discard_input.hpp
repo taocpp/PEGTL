25dfbc10b5f21207a0e290e1ae6c7d3a13a66241
Daniel Frey <d.frey@gmx.de>
2019-03-29 19:35:41 +0100

    New

---
+++ include/tao/pegtl/contrib/discard_input.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2019 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
+
+#ifndef TAO_PEGTL_CONTRIB_DISCARD_INPUT_HPP
+#define TAO_PEGTL_CONTRIB_DISCARD_INPUT_HPP
+
+#include "../apply_mode.hpp"
+#include "../config.hpp"
+#include "../match.hpp"
+#include "../rewind_mode.hpp"
+
+namespace TAO_PEGTL_NAMESPACE
+{
+   struct discard_input
+   {
+      template< typename Rule,
+                apply_mode A,
+                rewind_mode M,
+                template< typename... >
+                class Action,
+                template< typename... >
+                class Control,
+                typename Input,
+                typename... States >
+      [[nodiscard]] static bool match( Input& in, States&&... st )
+      {
+         const bool result = TAO_PEGTL_NAMESPACE::match< Rule, apply_mode::nothing, M, Action, Control >( in, st... );
+         in.discard();
+         return result;
+      }
+   };
+
+}  // namespace TAO_PEGTL_NAMESPACE
+
+#endif
---
+++ include/tao/pegtl/contrib/discard_input_on_success.hpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2019 Dr. Colin Hirsch and Daniel Frey
+// Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
+
+#ifndef TAO_PEGTL_CONTRIB_DISCARD_INPUT_ON_SUCCESS_HPP
+#define TAO_PEGTL_CONTRIB_DISCARD_INPUT_ON_SUCCESS_HPP
+
+#include "../apply_mode.hpp"
+#include "../config.hpp"
+#include "../match.hpp"
+#include "../rewind_mode.hpp"
+
+namespace TAO_PEGTL_NAMESPACE
+{
+   struct discard_input_on_success
+   {
+      template< typename Rule,
+                apply_mode A,
+                rewind_mode M,
+                template< typename... >
+                class Action,
+                template< typename... >
+                class Control,
+                typename Input,
+                typename... States >
+      [[nodiscard]] static bool match( Input& in, States&&... st )
+      {
+         const bool result = TAO_PEGTL_NAMESPACE::match< Rule, apply_mode::nothing, M, Action, Control >( in, st... );
+         if( result ) {
+            in.discard();
+         }
+         return result;
+      }
+   };
+
+}  // namespace TAO_PEGTL_NAMESPACE
+
+#endif
