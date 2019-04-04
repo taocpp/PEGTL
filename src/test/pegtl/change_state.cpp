19db1098caf81e37174635773c985b99329aa80d
Daniel Frey <d.frey@gmx.de>
2019-04-05 00:25:48 +0200

    Coverage

--- src/test/pegtl/change_state.cpp
+++ src/test/pegtl/change_state.cpp
@@ -3,14 +3,12 @@
 
 #include "test.hpp"
 
-#include <string>
-
 namespace TAO_PEGTL_NAMESPACE
 {
    // clang-format off
-   struct C : one< 'c' > {};
-   struct B : seq< one< 'b' >, C, one< 'b' > > {};
-   struct A : seq< one< 'a' >, B, one< 'a' > > {};
+   struct A : one< 'a' > {};
+   struct B : one< 'b' > {};
+   struct AB : seq< A, B > {};
    // clang-format on
 
    template< typename >
@@ -20,39 +18,86 @@ namespace TAO_PEGTL_NAMESPACE
    template<>
    struct my_action< A >
    {
-      static void apply0( std::string& s )
+      static void apply0( int& c )
       {
-         s += 'Y';
+         if( c != 0 ) {
+            throw std::runtime_error( "fail1" );
+         }
+         c = 1;
+      }
+   };
+
+   struct S
+   {
+      int v = 0;
+
+      template< typename Input >
+      explicit S( const Input& /*unused*/, int& c )
+      {
+         if( c == 5 ) {
+            v = 6;
+         }
+         else {
+            if( c != 1 ) {
+               throw std::runtime_error( "fail2" );
+            }
+            v = 2;
+         }
+      }
+
+      template< typename Input >
+      void success( const Input& /*unused*/, int& c )
+      {
+         if( v != 3 ) {
+            throw std::runtime_error( "fail3" );
+         }
+         c = 4;
       }
    };
 
    template<>
    struct my_action< B >
-      : change_states< int >
+      : change_state< S >
    {
-      template< typename Input >
-      static void success( const Input& /*unused*/, int& i, std::string& s )
+      static void apply0( S& s )
       {
-         s += std::to_string( i );
-      }
-   };
-
-   template<>
-   struct my_action< C >
-   {
-      static void apply0( int& i )
-      {
-         i = 42;
+         if( s.v != 2 ) {
+            throw std::runtime_error( "fail4" );
+         }
+         s.v = 3;
       }
    };
 
    void unit_test()
    {
-      memory_input in( "abcba", "" );
-      std::string s = "X";
-      const auto result = parse< A, my_action >( in, s );
-      TAO_PEGTL_TEST_ASSERT( result );
-      TAO_PEGTL_TEST_ASSERT( s == "X42Y" );
+      {
+         memory_input in( "ab", "" );
+         int c = 0;
+         const auto result = parse< AB, my_action >( in, c );
+         TAO_PEGTL_TEST_ASSERT( result );
+         TAO_PEGTL_TEST_ASSERT( c == 4 );
+      }
+      {
+         memory_input in( "a", "" );
+         int c = 0;
+         const auto result = parse< AB, my_action >( in, c );
+         TAO_PEGTL_TEST_ASSERT( !result );
+         TAO_PEGTL_TEST_ASSERT( c == 1 );
+      }
+      {
+         memory_input in( "b", "" );
+         int c = 0;
+         const auto result = parse< AB, my_action >( in, c );
+         TAO_PEGTL_TEST_ASSERT( !result );
+         TAO_PEGTL_TEST_ASSERT( c == 0 );
+      }
+      {
+         memory_input in( "ab", "" );
+         int c = 5;
+         const auto result = parse< disable< AB >, my_action >( in, c );
+         TAO_PEGTL_TEST_ASSERT( result );
+         TAO_PEGTL_TEST_ASSERT( c == 5 );
+      }
    }
 
 }  // namespace TAO_PEGTL_NAMESPACE
