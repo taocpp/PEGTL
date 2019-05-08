dc9b57a12d9ff40d16c876f8b84dcb4d168b37d9
Daniel Frey <d.frey@gmx.de>
2019-05-08 18:46:01 +0200

    Improve tests

--- src/test/pegtl/contrib_ascii_numeric_string.cpp
+++ src/test/pegtl/contrib_ascii_numeric_string.cpp
@@ -11,46 +11,48 @@ namespace TAO_PEGTL_NAMESPACE
    void test( const char* v )
    {
       memory_input in( v, "" );
-      const auto result = parse< seq< numeric_string< M >, eof > >( in );
+      const auto result = parse< seq< one< 'a' >, numeric_string< M >, one< 'b' >, eof > >( in );
       TAO_PEGTL_TEST_ASSERT( result == E );
    }
 
    void unit_test()
    {
-      test< 0, true >( "0" );
-      test< 0, false >( "1" );
-      test< 0, false >( "9" );
+      test< 0, true >( "a0b" );
+      test< 0, false >( "ab" );
+      test< 0, false >( "a1b" );
+      test< 0, false >( "a9b" );
 
-      test< 1, true >( "0" );
-      test< 1, true >( "1" );
-      test< 1, false >( "2" );
-      test< 1, false >( "9" );
+      test< 1, true >( "a0b" );
+      test< 1, true >( "a1b" );
+      test< 1, false >( "ab" );
+      test< 1, false >( "a2b" );
+      test< 1, false >( "a9b" );
 
-      test< 12, true >( "0" );
-      test< 12, true >( "1" );
-      test< 12, true >( "9" );
-      test< 12, true >( "10" );
-      test< 12, true >( "11" );
-      test< 12, true >( "12" );
-      test< 12, false >( "13" );
-      test< 12, false >( "120" );
-      test< 12, false >( "111" );
-      test< 12, false >( "00" );
-      test< 12, false >( "01" );
+      test< 12, true >( "a0b" );
+      test< 12, true >( "a1b" );
+      test< 12, true >( "a9b" );
+      test< 12, true >( "a10b" );
+      test< 12, true >( "a11b" );
+      test< 12, true >( "a12b" );
+      test< 12, false >( "a13b" );
+      test< 12, false >( "a120b" );
+      test< 12, false >( "a111b" );
+      test< 12, false >( "a00b" );
+      test< 12, false >( "a01b" );
 
-      test< 100, true >( "0" );
-      test< 100, true >( "1" );
-      test< 100, true >( "10" );
-      test< 100, true >( "99" );
-      test< 100, true >( "100" );
-      test< 100, false >( "101" );
+      test< 100, true >( "a0b" );
+      test< 100, true >( "a1b" );
+      test< 100, true >( "a10b" );
+      test< 100, true >( "a99b" );
+      test< 100, true >( "a100b" );
+      test< 100, false >( "a101b" );
 
-      test< 18446744073709551614ULL, true >( "18446744073709551614" );
-      test< 18446744073709551614ULL, false >( "18446744073709551615" );
+      test< 18446744073709551614ULL, true >( "a18446744073709551614b" );
+      test< 18446744073709551614ULL, false >( "a18446744073709551615b" );
 
-      test< 18446744073709551615ULL, true >( "18446744073709551615" );
-      test< 18446744073709551615ULL, false >( "18446744073709551616" );
-      test< 18446744073709551615ULL, false >( "98446744073709551614" );
+      test< 18446744073709551615ULL, true >( "a18446744073709551615b" );
+      test< 18446744073709551615ULL, false >( "a18446744073709551616b" );
+      test< 18446744073709551615ULL, false >( "a98446744073709551614b" );
    }
 
 }  // namespace TAO_PEGTL_NAMESPACE
