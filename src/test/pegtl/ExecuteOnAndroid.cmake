3b6018c28deaf33765ef1ea4e8e1f7a7d6949a9e
Joerg-Christian Boehme <joerg@chaosdorf.de>
2019-07-10 17:17:54 +0200

    Fix execute on android

--- src/test/pegtl/ExecuteOnAndroid.cmake
+++ src/test/pegtl/ExecuteOnAndroid.cmake
@@ -1,4 +1,12 @@
 
+if("${ANDROID_NDK}" STREQUAL "")
+    message(FATAL_ERROR "cmake parameter ANDROID_NDK is not set!")
+endif()
+
+if(NOT EXISTS "${ANDROID_NDK}/../platform-tools/adb")
+    message(FATAL_ERROR "adb is not found in ${ANDROID_NDK}/../platform-tools")
+endif()
+
 get_filename_component(UNITTEST_FILENAME ${UNITTEST} NAME)
 message(STATUS "Cleanup /data/local/tmp ...")
 execute_process(COMMAND ${ANDROID_NDK}/../platform-tools/adb shell "rm -r /data/local/tmp/*" OUTPUT_QUIET)
@@ -17,13 +25,6 @@ endif()
 message(STATUS "Push ${UNITTEST} to android ...")
 execute_process(COMMAND ${ANDROID_NDK}/../platform-tools/adb push ${UNITTEST} /data/local/tmp/ OUTPUT_QUIET)
 message(STATUS "Execute ${UNITTEST_FILENAME} on android ...")
-execute_process(
-   COMMAND ${ANDROID_NDK}/../platform-tools/adb shell "cd /data/local/tmp;ls -R"
-   RESULT_VARIABLE _RESULT
-   OUTPUT_VARIABLE _OUT
-   ERROR_VARIABLE _ERR
-)
-message(STATUS "Result: ${_RESULT} \nOutput: ${_OUT}\nError: ${_ERR}")
 
 execute_process(
    COMMAND ${ANDROID_NDK}/../platform-tools/adb shell "cd /data/local/tmp;ls -R;su root sh -c 'LD_LIBRARY_PATH=/data/local/tmp/lib TMPDIR=/data/local/tmp HOME=/data/local/tmp ./${UNITTEST_FILENAME};echo exit code $?'"
@@ -32,10 +33,8 @@ execute_process(
    ERROR_VARIABLE _ERR
 )
 
-message(STATUS "Result: ${_RESULT} \nOutput: ${_OUT}\nError: ${_ERR}")
-
 if(_RESULT)
-    message(FATAL_ERROR "Execution of ${UNITTEST_FILENAME} failed: ${_RESULT}")
+    message(FATAL_ERROR "Execution of ${UNITTEST_FILENAME} failed")
 else()
     string(REGEX MATCH "exit code ([0-9]+)" _EXIT_CODE ${_OUT})
     if(NOT "${CMAKE_MATCH_1}" EQUAL 0)
