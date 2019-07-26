d17aa5601cd98fd61f3c859a3bc5d8c239b2f84a
Daniel Frey <d.frey@gmx.de>
2019-07-26 22:27:17 +0200

    Coverage

--- src/test/pegtl/contrib_http.cpp
+++ src/test/pegtl/contrib_http.cpp
@@ -60,6 +60,16 @@ namespace tao
             TAO_PEGTL_TEST_ASSERT( state == "a" );
          }
          {
+            std::string state;
+            string_input<> in( "\r\n\r\n", __FUNCTION__ );
+            TAO_PEGTL_TEST_THROWS( parse< GRAMMAR, chunked_action >( in, state ) );
+         }
+         {
+            std::string state;
+            string_input<> in( "1\r\n", __FUNCTION__ );
+            TAO_PEGTL_TEST_THROWS( parse< GRAMMAR, chunked_action >( in, state ) );
+         }
+         {
             string_input<> in( "01\r\nX\r\n1a\r\nabcdefghijklmnopqrstuvwxyz\r\n0\r\n\r\n", __FUNCTION__ );
             TAO_PEGTL_TEST_ASSERT( parse< GRAMMAR >( in ) );
          }
@@ -70,7 +80,7 @@ namespace tao
          }
          {
             std::string state;
-            string_input<> in( "01\r\nX\r\n1a\r\nabcdefghijklmnopqrstuvwxyz\r\n0\r\n\r\n", __FUNCTION__ );
+            string_input<> in( "01\r\nX\r\n1A\r\nabcdefghijklmnopqrstuvwxyz\r\n0\r\n\r\n", __FUNCTION__ );
             TAO_PEGTL_TEST_ASSERT( parse< GRAMMAR, chunked_action >( in, state ) );
             TAO_PEGTL_TEST_ASSERT( state == "ababa" );
          }
