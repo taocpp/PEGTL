881699e1c977974771bf2aff1fc7d8f5fae2b357
Rich Newman <rnewman@parasoft.com>
2019-08-26 18:12:36 -0700

    Allow printing graphs of subsets of the whole parse tree. Also allows, e.g. parse_tree::fold_one::transform(root)

--- include/tao/pegtl/contrib/parse_tree_to_dot.hpp
+++ include/tao/pegtl/contrib/parse_tree_to_dot.hpp
@@ -92,9 +92,8 @@ namespace TAO_PEGTL_NAMESPACE::parse_tree
 
    void print_dot( std::ostream& os, const parse_tree::node& n )
    {
-      assert( n.is_root() );
       os << "digraph parse_tree\n{\n";
-      internal::print_dot_node( os, n, "ROOT" );
+      internal::print_dot_node( os, n, n.is_root() ? "ROOT": n.type );
       os << "}\n";
    }
 
