762296f95de2bac8b73eea83dac67ffc381dd9fd
Daniel Frey <d.frey@gmx.de>
2020-04-07 20:01:40 +0200

    Simplify

--- include/tao/pegtl/internal/sor.hpp
+++ include/tao/pegtl/internal/sor.hpp
@@ -20,9 +20,6 @@ namespace TAO_PEGTL_NAMESPACE::internal
    template< typename... Rules >
    struct sor;
 
-   template< typename... Rules >
-   struct sor_impl;
-
    template<>
    struct sor<>
       : failure
@@ -33,15 +30,24 @@ namespace TAO_PEGTL_NAMESPACE::internal
 
    template< typename... Rules >
    struct sor
-      : sor_impl< std::index_sequence_for< Rules... >, Rules... >
    {
       using rule_t = sor;
       using subs_t = rule_list< Rules... >;
-   };
 
-   template< std::size_t... Indices, typename... Rules >
-   struct sor_impl< std::index_sequence< Indices... >, Rules... >
-   {
+      template< apply_mode A,
+                rewind_mode M,
+                template< typename... >
+                class Action,
+                template< typename... >
+                class Control,
+                std::size_t... Indices,
+                typename Input,
+                typename... States >
+      [[nodiscard]] static bool match( std::index_sequence< Indices... >, Input& in, States&&... st )
+      {
+         return ( Control< Rules >::template match< A, ( ( Indices == ( sizeof...( Rules ) - 1 ) ) ? M : rewind_mode::required ), Action, Control >( in, st... ) || ... );
+      }
+
       template< apply_mode A,
                 rewind_mode M,
                 template< typename... >
@@ -52,7 +58,7 @@ namespace TAO_PEGTL_NAMESPACE::internal
                 typename... States >
       [[nodiscard]] static bool match( Input& in, States&&... st )
       {
-         return ( Control< Rules >::template match< A, ( ( Indices == ( sizeof...( Rules ) - 1 ) ) ? M : rewind_mode::required ), Action, Control >( in, st... ) || ... );
+         return match< A, M, Action, Control >( std::index_sequence_for< Rules... >(), in, st... );
       }
    };
 
