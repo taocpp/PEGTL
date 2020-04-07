f807a3f5ce6cf1f5ce9a781ee6d58ae2850d02f6
Daniel Frey <d.frey@gmx.de>
2020-04-08 01:48:58 +0200

    Reduce template instantiation depth

--- include/tao/pegtl/visit.hpp
+++ include/tao/pegtl/visit.hpp
@@ -17,11 +17,24 @@ namespace TAO_PEGTL_NAMESPACE
       template< typename Type, typename... Types >
       inline constexpr bool contains = ( std::is_same_v< Type, Types > || ... );
 
-      template< template< typename... > class Func, typename Todo, typename Done, typename = void >
-      struct visitor;
+      template< typename Result, typename Todo, typename Done, typename = void >
+      struct filter
+      {
+         using type = Result;
+      };
 
-      template< template< typename... > class Func, typename... Done >
-      struct visitor< Func, rule_list<>, rule_list< Done... > >
+      template< typename... Result, typename Rule, typename... Todo, typename... Done >
+      struct filter< rule_list< Result... >, rule_list< Rule, Todo... >, rule_list< Done... >, std::enable_if_t< contains< Rule, Done... > > >
+         : filter< rule_list< Result... >, rule_list< Todo... >, rule_list< Done... > >
+      {};
+
+      template< typename... Result, typename Rule, typename... Todo, typename... Done >
+      struct filter< rule_list< Result... >, rule_list< Rule, Todo... >, rule_list< Done... >, std::enable_if_t< !contains< Rule, Done... > > >
+         : filter< rule_list< Result..., Rule >, rule_list< Todo... >, rule_list< Done..., Rule > >
+      {};
+
+      template< template< typename... > class Func, typename Todo, typename Done >
+      struct visitor
       {
          template< typename... Args >
          static void visit( Args&&... /*unused*/ )
@@ -29,12 +42,7 @@ namespace TAO_PEGTL_NAMESPACE
       };
 
       template< template< typename... > class Func, typename Rule, typename... Todo, typename... Done >
-      struct visitor< Func, rule_list< Rule, Todo... >, rule_list< Done... >, std::enable_if_t< contains< Rule, Done... > > >
-         : visitor< Func, rule_list< Todo... >, rule_list< Done... > >
-      {};
-
-      template< template< typename... > class Func, typename Rule, typename... Todo, typename... Done >
-      struct visitor< Func, rule_list< Rule, Todo... >, rule_list< Done... >, std::enable_if_t< !contains< Rule, Done... > > >
+      struct visitor< Func, rule_list< Rule, Todo... >, rule_list< Done... > >
       {
          template< typename... Args >
          static void visit( Args&&... args )
@@ -47,7 +55,9 @@ namespace TAO_PEGTL_NAMESPACE
          static void visit_impl( rule_list< Subs... > /*unused*/, Args&&... args )
          {
             Func< Rule, Subs... >::call( std::forward< Args >( args )... );
-            visitor< Func, rule_list< Subs..., Todo... >, rule_list< Rule, Done... > >::visit( std::forward< Args >( args )... );
+            using NextDone = rule_list< Rule, Done... >;
+            using NextTodo = typename filter< rule_list<>, rule_list< Subs..., Todo... >, NextDone >::type;
+            visitor< Func, NextTodo, NextDone >::visit( std::forward< Args >( args )... );
          }
       };
 
