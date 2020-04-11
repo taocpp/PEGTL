af9bbc78229d695cdb692fcb1af55d129bef5ec0
Daniel Frey <d.frey@gmx.de>
2020-04-11 11:08:19 +0200

    Fix GCC

--- include/tao/pegtl/contrib/parse_tree.hpp
+++ include/tao/pegtl/contrib/parse_tree.hpp
@@ -280,8 +280,40 @@ namespace TAO_PEGTL_NAMESPACE::parse_tree
       template< typename Node, template< typename... > class Selector, template< typename... > class Control >
       template< typename Rule >
       struct make_control< Node, Selector, Control >::state_handler< Rule, false, false >
-         : make_control< Node, Selector, Control >::state_handler< Rule, false, true >
+         : Control< Rule >
       {
+         template< typename Input, typename... States >
+         static void start( const Input& in, state< Node >& /*unused*/, States&&... st ) noexcept( noexcept( Control< Rule >::start( in, st... ) ) )
+         {
+            Control< Rule >::start( in, st... );
+         }
+
+         template< typename Input, typename... States >
+         static void success( const Input& in, state< Node >& /*unused*/, States&&... st ) noexcept( noexcept( Control< Rule >::success( in, st... ) ) )
+         {
+            Control< Rule >::success( in, st... );
+         }
+
+         template< typename Input, typename... States >
+         static void failure( const Input& in, state< Node >& /*unused*/, States&&... st ) noexcept( noexcept( Control< Rule >::failure( in, st... ) ) )
+         {
+            Control< Rule >::failure( in, st... );
+         }
+
+         template< template< typename... > class Action, typename Iterator, typename Input, typename... States >
+         static auto apply( const Iterator& begin, const Input& in, state< Node >& /*unused*/, States&&... st ) noexcept( noexcept( Control< Rule >::template apply< Action >( begin, in, st... ) ) )
+            -> decltype( Control< Rule >::template apply< Action >( begin, in, st... ) )
+         {
+            Control< Rule >::template apply< Action >( begin, in, st... );
+         }
+
+         template< template< typename... > class Action, typename Input, typename... States >
+         static auto apply0( const Input& in, state< Node >& /*unused*/, States&&... st ) noexcept( noexcept( Control< Rule >::template apply0< Action >( in, st... ) ) )
+            -> decltype( Control< Rule >::template apply0< Action >( in, st... ) )
+         {
+            Control< Rule >::template apply0< Action >( in, st... );
+         }
+
          template< apply_mode A,
                    rewind_mode M,
                    template< typename... >
