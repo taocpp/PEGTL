518204c0be42a14d50ef5baecacc823322d25a4a
Daniel Frey <d.frey@gmx.de>
2020-04-13 01:52:09 +0200

    Reduce template instantiation depth

--- include/tao/pegtl/visit.hpp
+++ include/tao/pegtl/visit.hpp
@@ -27,31 +27,49 @@ namespace TAO_PEGTL_NAMESPACE
          : filter< rule_list< Rules... >, std::conditional_t< contains< Rule, Todo..., Done... >, rule_list< Todo... >, rule_list< Rule, Todo... > >, rule_list< Done... > >
       {};
 
-      template< template< typename... > class Func, typename Todo, typename Done >
-      struct visitor
+      template< typename Rules, typename Todo, typename Done >
+      using filter_t = typename filter< Rules, Todo, Done >::type;
+
+      template< typename... >
+      struct concat
       {
-         template< typename... Args >
-         static void visit( Args&&... /*unused*/ )
-         {}
+         using type = rule_list<>;
       };
 
-      template< template< typename... > class Func, typename Rule, typename... Todo, typename... Done >
-      struct visitor< Func, rule_list< Rule, Todo... >, rule_list< Done... > >
+      template< typename RuleList >
+      struct concat< RuleList >
+      {
+         using type = RuleList;
+      };
+
+      template< typename... R0, typename... R1, typename... Tail >
+      struct concat< rule_list< R0... >, rule_list< R1... >, Tail... >
+         : concat< rule_list< R0..., R1... >, Tail... >
+      {};
+
+      template< typename... Ts >
+      using concat_t = typename concat< Ts... >::type;
+
+      template< template< typename... > class Func, typename Done, typename... Rules >
+      struct visitor
       {
          template< typename... Args >
          static void visit( Args&&... args )
          {
-            visit_impl( typename Rule::subs_t(), args... );
+            ( Func< Rules >::visit( args... ), ... );
+            using NextDone = concat_t< rule_list< Rules... >, Done >;
+            using NextSubs = concat_t< typename Rules::subs_t... >;
+            using NextTodo = filter_t< NextSubs, rule_list<>, NextDone >;
+            if constexpr( !std::is_same_v< NextTodo, empty_list > ) {
+               visit_next< NextDone >( NextTodo(), args... );
+            }
          }
 
       private:
-         template< typename... Subs, typename... Args >
-         static void visit_impl( rule_list< Subs... > /*unused*/, Args&&... args )
+         template< typename NextDone, typename... NextTodo, typename... Args >
+         static void visit_next( rule_list< NextTodo... > /*unused*/, Args&&... args )
          {
-            Func< Rule, Subs... >::visit( args... );
-            using NextDone = rule_list< Rule, Done... >;
-            using NextTodo = typename filter< rule_list< Subs... >, rule_list< Todo... >, NextDone >::type;
-            visitor< Func, NextTodo, NextDone >::visit( args... );
+            visitor< Func, NextDone, NextTodo... >::visit( args... );
          }
       };
 
@@ -60,7 +78,7 @@ namespace TAO_PEGTL_NAMESPACE
    template< template< typename... > class Func, typename Rule, typename... Args >
    void visit( Args&&... args )
    {
-      internal::visitor< Func, rule_list< Rule >, rule_list<> >::visit( args... );
+      internal::visitor< Func, rule_list<>, Rule >::visit( args... );
    }
 
 }  // namespace TAO_PEGTL_NAMESPACE
