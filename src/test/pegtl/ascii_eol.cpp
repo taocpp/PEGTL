69a77d15cbf37fd3827115e4a9f9cf6b5a778efb
Daniel Frey <d.frey@gmx.de>
2020-04-22 19:49:16 +0200

    clang-format

--- src/test/pegtl/ascii_eol.cpp
+++ src/test/pegtl/ascii_eol.cpp
@@ -2,8 +2,8 @@
 // Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
 
 #include "test.hpp"
-#include "verify_meta.hpp"
 #include "verify_char.hpp"
+#include "verify_meta.hpp"
 #include "verify_rule.hpp"
 
 namespace TAO_PEGTL_NAMESPACE
--- src/test/pegtl/contrib_raw_string.cpp
+++ src/test/pegtl/contrib_raw_string.cpp
@@ -2,8 +2,8 @@
 // Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
 
 #include "test.hpp"
-#include "verify_meta.hpp"
 #include "verify_fail.hpp"
+#include "verify_meta.hpp"
 
 #include <tao/pegtl/contrib/raw_string.hpp>
 
