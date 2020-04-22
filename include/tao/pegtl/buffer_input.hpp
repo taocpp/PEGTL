0e85e992fdf707dd7c751912532a1eea5219dd25
Daniel Frey <d.frey@gmx.de>
2020-04-22 18:43:55 +0200

    Fix #202

--- include/tao/pegtl/buffer_input.hpp
+++ include/tao/pegtl/buffer_input.hpp
@@ -149,7 +149,7 @@ namespace TAO_PEGTL_NAMESPACE
          if( m_current.data + amount > m_buffer.get() + m_maximum ) {
             throw std::overflow_error( "require beyond end of buffer" );
          }
-         if( const auto r = m_reader( m_end, ( std::min )( buffer_free_after_end(), ( std::max )( amount, Chunk ) ) ) ) {
+         if( const auto r = m_reader( m_end, ( std::min )( buffer_free_after_end(), ( std::max )( amount - buffer_occupied(), Chunk ) ) ) ) {
             m_end += r;
          }
       }
