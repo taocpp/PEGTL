acc772b4a5ef913c70ab33d043aac5a84e9bbb05
Daniel Frey <d.frey@gmx.de>
2020-04-27 23:31:45 +0200

    Add exception handling

--- src/example/pegtl/json_parse.cpp
+++ src/example/pegtl/json_parse.cpp
@@ -1,6 +1,9 @@
 // Copyright (c) 2014-2020 Dr. Colin Hirsch and Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/PEGTL/
 
+#include <iomanip>
+#include <iostream>
+
 #include <tao/pegtl.hpp>
 
 #include "json_errors.hpp"
@@ -11,8 +14,21 @@ using grammar = must< json::text, eof >;
 int main( int argc, char** argv )  // NOLINT(bugprone-exception-escape)
 {
    for( int i = 1; i < argc; ++i ) {
-      argv_input in( argv, i );
-      parse< grammar, nothing, examples::control >( in );
+      try {
+         argv_input in( argv, i );
+         try {
+            parse< grammar, nothing, examples::control >( in );
+         }
+         catch( const parse_error& e ) {
+            const auto p = e.positions.front();
+            std::cerr << e.what() << '\n'
+                      << in.line_at( p ) << '\n'
+                      << std::setw( p.byte_in_line ) << ' ' << '^' << std::endl;
+         }
+      }
+      catch( const std::exception& e ) {
+         std::cerr << e.what() << std::endl;
+      }
    }
    return 0;
 }
