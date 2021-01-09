9cdfcf2bc8454f2bc462d9b0e2433e3eab33c0fe
Daniel Frey <d.frey@gmx.de>
2021-01-09 18:20:40 +0100

    Simplify, allow disabling exceptions

--- src/test/pegtl/change_action_and_states.cpp
+++ src/test/pegtl/change_action_and_states.cpp
@@ -24,9 +24,7 @@ namespace TAO_PEGTL_NAMESPACE
    {
       static void apply0( int& c )
       {
-         if( c != 0 ) {
-            throw std::runtime_error( "fail1" );  // LCOV_EXCL_LINE
-         }
+         TAO_PEGTL_TEST_ASSERT( c == 0 );
          c = 1;
       }
    };
@@ -38,18 +36,14 @@ namespace TAO_PEGTL_NAMESPACE
       // not called because my_action_2 is active
       static void apply0( int& /*v*/ )
       {
-         throw std::runtime_error( "fail2" );  // LCOV_EXCL_LINE
+         TAO_PEGTL_TEST_UNREACHABLE;
       }
 
       template< typename ParseInput >
       static void success( const ParseInput& /*unused*/, int& v, int& c )
       {
-         if( v != 2 ) {
-            throw std::runtime_error( "fail3" );  // LCOV_EXCL_LINE
-         }
-         if( c != 1 ) {
-            throw std::runtime_error( "fail4" );  // LCOV_EXCL_LINE
-         }
+         TAO_PEGTL_TEST_ASSERT( v == 2 );
+         TAO_PEGTL_TEST_ASSERT( c == 1 );
          c = 3;
       }
    };
@@ -59,7 +53,7 @@ namespace TAO_PEGTL_NAMESPACE
    {
       static void apply0( int& /*c*/ )
       {
-         throw std::runtime_error( "fail5" );  // LCOV_EXCL_LINE
+         TAO_PEGTL_TEST_UNREACHABLE;
       }
    };
 
@@ -68,9 +62,7 @@ namespace TAO_PEGTL_NAMESPACE
    {
       static void apply0( int& v )
       {
-         if( v != 0 ) {
-            throw std::runtime_error( "fail6" );  // LCOV_EXCL_LINE
-         }
+         TAO_PEGTL_TEST_ASSERT( v == 0 );
          v = 2;
       }
    };
