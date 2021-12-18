ac2d215e6ff4f08264f9776b45e2352904ec7093
Daniel Frey <d.frey@gmx.de>
2021-12-18 11:00:09 +0100

    Cleanup

--- include/tao/pegtl/contrib/trace.hpp
+++ include/tao/pegtl/contrib/trace.hpp
@@ -100,10 +100,9 @@ namespace TAO_PEGTL_NAMESPACE
       }
 
       template< typename Rule, typename ParseInput, typename... States >
-      void start( const ParseInput& in, States&&... /*unused*/ )
+      void start( const ParseInput& /*unused*/, States&&... /*unused*/ )
       {
          std::cerr << '#' << std::setw( indent() - 1 ) << ++m_count << TracerTraits::ansi_rule << demangle< Rule >() << TracerTraits::ansi_reset << '\n';
-         const auto p = in.position();
          m_stack.push_back( m_count );
       }
 
