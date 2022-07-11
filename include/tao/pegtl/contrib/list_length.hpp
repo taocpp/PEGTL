38f3672d679c995e859f7255acb9b9dd24988adf
qawbecrdtey <qawbecrdtey@naver.com>
2022-07-11 14:22:01 +0900

    Added implementation of list_min_max.
    This allows lists of length 0 and 1.

--- include/tao/pegtl/contrib/list_length.hpp
+++ include/tao/pegtl/contrib/list_length.hpp
@@ -24,7 +24,7 @@ namespace tao::pegtl
       template< typename Rule, typename Sep, int64_t len >
       struct list_length
       {
-         static_assert( len > -2, "The length should be non-negative, or -1 for dynamic-length list.");
+         static_assert( len > -2, "The length should be non-negative, or -1 for dynamic-length list." );
 
          using rule_t = list_length;
          using subs_t = type_list< Rule, Sep >;
@@ -46,12 +46,34 @@ namespace tao::pegtl
                auto m = in.template auto_rewind< M >();
                using m_t = decltype( m );
                // clang-format off
-            return m( ( Control< Rule >::template match< A, m_t::next_rewind_mode, Action, Control >( in, std::forward< States >( st )... ) ) &&
-                      ( Control< Sep >::template match< A, m_t::next_rewind_mode, Action, Control >( in, std::forward< States >( st )... ) ) &&
-                      ( Control< list_length< Rule, Sep, len - 1 > >::template match< A, m_t::next_rewind_mode, Action, Control >( in, std::forward< States >( st )... ) ) );
+               return m( ( Control< Rule >::template match< A, m_t::next_rewind_mode, Action, Control >( in, std::forward< States >( st )... ) ) &&
+                         ( Control< Sep >::template match< A, m_t::next_rewind_mode, Action, Control >( in, std::forward< States >( st )... ) ) &&
+                         ( Control< list_length< Rule, Sep, len - 1 > >::template match< A, m_t::next_rewind_mode, Action, Control >( in, std::forward< States >( st )... ) ) );
                // clang-format on
             }
          }
+      };
+
+      template< std::size_t Min, std::size_t Max, typename Rule, typename Sep >
+      struct list_min_max;
+
+      template< typename Rule, typename Sep >
+      struct list_min_max< 0, 0, Rule, Sep >
+         : success
+      {};
+
+      template< std::size_t Max, typename Rule, typename Sep >
+      struct list_min_max< 0, Max, Rule, Sep >
+         : opt< list_min_max< 1, Max, Rule, Sep > >
+      {
+         static_assert( Max > 0 );
+      };
+
+      template< std::size_t Min, std::size_t Max, typename Rule, typename Sep >
+      struct list_min_max
+         : seq< Rule, rep_min_max< Min - 1, Max - 1, seq< Sep, Rule > > >
+      {
+         static_assert( Min > 0 );
       };
 
    }  // namespace internal
@@ -63,7 +85,10 @@ namespace tao::pegtl
    template< typename Rule, typename Sep, typename Pad > struct list< Rule, Sep, Pad, dynamic > : internal::list< Rule, internal::pad< Sep, Pad > > {};
    template< typename Rule, typename Sep > struct list< Rule, Sep, void, dynamic > : internal::list< Rule, Sep > {};
    template< typename Rule, typename Sep, int64_t len > struct list< Rule, Sep, void, len > : internal::list_length< Rule, Sep, len > {};
-   // clang-format on
+
+   template< std::size_t Min, std::size_t Max, typename Rule, typename Sep, typename Pad = void > struct list_min_max : internal::list_min_max< Min, Max, Rule, internal::pad< Sep, Pad > > {};
+   template< std::size_t Min, std::size_t Max, typename Rule, typename Sep > struct list_min_max< Min, Max, Rule, Sep, void > : internal::list_min_max< Min, Max, Rule, Sep > {};
+   //clang-format on
 
 }  // namespace tao::pegtl
 
--- src/example/pegtl/list_length.cpp
+++ src/example/pegtl/list_length.cpp
@@ -44,6 +44,47 @@ namespace list_length_example
 
 }  // namespace list_length_example
 
+namespace list_min_max_example
+{
+   namespace pegtl = tao::pegtl;
+
+   struct digits : pegtl::plus< pegtl::digit >
+   {};
+
+   template< std::size_t Min, std::size_t Max >
+   struct list_min_max_expression : pegtl::list_min_max< Min, Max, digits, pegtl::one< ',' >, pegtl::one< ' ' > >
+   {};
+
+   template< std::size_t M >
+   struct grammar : pegtl::seq< list_min_max_expression< M, M + 1 >, pegtl::eolf >
+   {};
+
+   template< typename Rule >
+   struct action
+   {};
+
+   template<>
+   struct action< digits >
+   {
+      template< typename ActionInput >
+      static void apply( const ActionInput& in, uint64_t& val )
+      {
+         val += std::stoull( in.string() );
+      }
+   };
+
+   template< uint64_t M >
+   struct action< grammar< M > >
+   {
+      template< typename... States >
+      static void apply( States&&... /*unused*/ )
+      {
+         std::cout << "Hello from " << M << "." << std::endl;
+      }
+   };
+
+}  // namespace list_min_max_example
+
 static std::string generate_correct_input( uint64_t len )
 {
    if( len == 0 ) {
@@ -52,10 +93,10 @@ static std::string generate_correct_input( uint64_t len )
    if( len == 1 ) {
       return "1";
    }
-   return std::to_string( len ) + "," + std::string(     len / 31, ' ' ) + generate_correct_input( len - 1 );
+   return std::to_string( len ) + "," + std::string( len / 31, ' ' ) + generate_correct_input( len - 1 );
 }
 
-static std::string generate_incorrect_input( uint64_t len )
+static std::string generate_incorrect_input1( uint64_t len )
 {
    if( len == 0 ) {
       return "0";
@@ -63,43 +104,98 @@ static std::string generate_incorrect_input( uint64_t len )
    return generate_correct_input( len ) + ",0";
 }
 
-#define TEST_CASE( FUN, LEN )                                                                                  \
-   do {                                                                                                        \
-      auto input_string = FUN( LEN );                                                                          \
-      tao::pegtl::memory_input in( input_string.c_str(), "" );                                                 \
-      uint64_t sum = 0;                                                                                        \
-                                                                                                               \
-      if( tao::pegtl::parse< list_length_example::grammar< LEN >, list_length_example::action >( in, sum ) ) { \
-         std::cout << "string \"" << input_string << "\" parsed successfully." << std::endl;                   \
-         std::cout << "sum is: " << sum << "\n"                                                                \
-                   << std::endl;                                                                               \
-      }                                                                                                        \
-      else {                                                                                                   \
-         std::cerr << "Failed to parse \"" << input_string << "\"." << std::endl;                              \
-      }                                                                                                        \
+static std::string generate_incorrect_input2( uint64_t len )
+{
+   if( len == 0 ) {
+      return "0,1";
+   }
+   return generate_correct_input( len ) + ",0,1";
+}
+
+#define TEST_CASE( NAMESPACE, FUN, LEN )                                                          \
+   do {                                                                                           \
+      auto input_string = FUN( LEN );                                                             \
+      tao::pegtl::memory_input in( input_string.c_str(), "" );                                    \
+      uint64_t sum = 0;                                                                           \
+                                                                                                  \
+      if( tao::pegtl::parse< NAMESPACE::grammar< LEN >, NAMESPACE::action >( in, sum ) ) {        \
+         std::cout << "From " << #NAMESPACE << ":\n";                                             \
+         std::cout << "string \"" << input_string << "\" parsed successfully." << std::endl;      \
+         std::cout << "sum is: " << sum << "\n"                                                   \
+                   << std::endl;                                                                  \
+      }                                                                                           \
+      else {                                                                                      \
+         std::cerr << #NAMESPACE << ": Failed to parse \"" << input_string << "\"." << std::endl; \
+      }                                                                                           \
    } while( 0 )
 
 int main()
 {
-   TEST_CASE( generate_correct_input, 0 );
-   TEST_CASE( generate_correct_input, 1 );
-   TEST_CASE( generate_correct_input, 2 );
-   TEST_CASE( generate_correct_input, 3 );
-   TEST_CASE( generate_correct_input, 4 );
-   TEST_CASE( generate_correct_input, 5 );
-   TEST_CASE( generate_correct_input, 6 );
-   TEST_CASE( generate_correct_input, 24 );
-   TEST_CASE( generate_correct_input, 42 );
-   TEST_CASE( generate_correct_input, 100 );
+   TEST_CASE( list_length_example, generate_correct_input, 0 );
+   TEST_CASE( list_length_example, generate_correct_input, 1 );
+   TEST_CASE( list_length_example, generate_correct_input, 2 );
+   TEST_CASE( list_length_example, generate_correct_input, 3 );
+   TEST_CASE( list_length_example, generate_correct_input, 4 );
+   TEST_CASE( list_length_example, generate_correct_input, 5 );
+   TEST_CASE( list_length_example, generate_correct_input, 6 );
+   TEST_CASE( list_length_example, generate_correct_input, 24 );
+   TEST_CASE( list_length_example, generate_correct_input, 42 );
+   TEST_CASE( list_length_example, generate_correct_input, 100 );
 
-   TEST_CASE( generate_incorrect_input, 0 );
-   TEST_CASE( generate_incorrect_input, 1 );
-   TEST_CASE( generate_incorrect_input, 2 );
-   TEST_CASE( generate_incorrect_input, 3 );
-   TEST_CASE( generate_incorrect_input, 4 );
-   TEST_CASE( generate_incorrect_input, 5 );
-   TEST_CASE( generate_incorrect_input, 6 );
-   TEST_CASE( generate_incorrect_input, 24 );
-   TEST_CASE( generate_incorrect_input, 42 );
-   TEST_CASE( generate_incorrect_input, 100 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 0 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 1 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 2 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 3 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 4 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 5 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 6 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 24 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 42 );
+   TEST_CASE( list_length_example, generate_incorrect_input1, 100 );
+
+   TEST_CASE( list_length_example, generate_incorrect_input2, 0 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 1 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 2 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 3 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 4 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 5 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 6 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 24 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 42 );
+   TEST_CASE( list_length_example, generate_incorrect_input2, 100 );
+
+   TEST_CASE( list_min_max_example, generate_correct_input, 0 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 1 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 2 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 3 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 4 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 5 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 6 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 24 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 42 );
+   TEST_CASE( list_min_max_example, generate_correct_input, 100 );
+
+   // works
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 0 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 1 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 2 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 3 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 4 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 5 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 6 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 24 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 42 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input1, 100 );
+
+   // fails
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 0 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 1 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 2 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 3 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 4 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 5 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 6 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 24 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 42 );
+   TEST_CASE( list_min_max_example, generate_incorrect_input2, 100 );
 }
