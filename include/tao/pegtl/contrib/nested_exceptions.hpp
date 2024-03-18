d9aa34466768082d2502a66c70b64b46f4e55ddf
Dr. Colin Hirsch <github@colin-hirsch.net>
2024-03-18 20:39:13 +0100

    Add to and test nested exception support.

--- include/tao/pegtl/contrib/nested_exceptions.hpp
+++ include/tao/pegtl/contrib/nested_exceptions.hpp
@@ -15,7 +15,7 @@
 #include "../config.hpp"
 #include "../parse_error.hpp"
 
-// Various utility functions and classes to handle nested exceptions given that parse_nested() now uses them.
+// Some utility functions and classes to handle nested exceptions given that parse_nested() now uses them.
 // At this point it's not clear yet how many, and which, of these will become and stay part of the PEGTL.
 
 namespace TAO_PEGTL_NAMESPACE
@@ -31,13 +31,13 @@ namespace TAO_PEGTL_NAMESPACE
          template< typename Processor, typename Caught, typename Visitor >
          static void rethrow( const Caught& caught, Visitor&& /*unused*/, const std::size_t /*unused*/ )
          {
-            std::rethrow_if_nested( caught );
+            std::rethrow_if_nested( caught );  // For the other/inner exceptions.
          }
 
          template< typename Processor, typename Visitor >
          static void rethrow( const std::exception_ptr& caught, Visitor&& /*unused*/, const std::size_t /*unused*/ )
          {
-            std::rethrow_exception( caught );
+            std::rethrow_exception( caught );  // For the first/outer exception when supplied as exception pointer.
          }
       };
 
@@ -78,13 +78,34 @@ namespace TAO_PEGTL_NAMESPACE
          {
             Rethrower::template rethrow< Processor >( ptr, visitor, 0 );
          }
+
+         template< typename Exception, typename Visitor >
+         static void inspect( const Exception& exception, Visitor&& visitor )
+         {
+            Processor::template process( exception, visitor, 0 );
+         }
       };
 
    }  // namespace internal
 
-   // Exceptions in an inheritance hierarchy MUST be listed from general to specific,
+   // Exceptions in an inheritance hierarchy MUST be listed FROM GENERAL TO SPECIFIC,
    // e.g. inspect< std::exception, std::runtime_error >, otherwise the more specific
-   // exceptions will be caught by the handler for the more general case.
+   // exceptions will be erroneously caught by the handler for the more general case.
+
+   // The visitor is called once for every exception, including the first/outer one.
+   // The visitor is called first with the inner-most exception, i.e. the one that
+   // was thrown first from furthest down/inside the call stack.
+   // The visitor is called last with the outer-most exception, i.e. the one that was
+   // caught in the try-catch that is calling inspect().
+
+   // The second argument to the visitor, the std::size_t with the nesting level of
+   // the exception that is passed as first argument, counts down from the inner-most
+   // to the outer-most exception, i.e. it counts how deep down the current exception
+   // is along the singly-linked list formed by the nested exceptions.
+
+   // For example, when an exception E is caught, and E has exception F as nested
+   // exception, then inspect( E, V ) for some visitor V will first call V( F, 1 )
+   // and then V( E, 0 ).
 
    template< typename... Exceptions, typename Visitor >
    void inspect( Visitor&& visitor )
@@ -98,10 +119,31 @@ namespace TAO_PEGTL_NAMESPACE
       internal::inspector< Exceptions... >::inspect( ptr, visitor );
    }
 
+   // NOTE: The following overload passes the outer exception to the visitor with its
+   // static type, Exception! This might not be the expected or desired behaviour...
+
+   template< typename... Exceptions, typename Exception, typename Visitor >
+   void inspect( const Exception& exception, Visitor&& visitor )
+   {
+      internal::inspector< Exceptions... >::inspect( exception, visitor );
+   }
+
+   // The visitor is supplied as last argument to the inspect() functions because it
+   // seems to be a good idea to have lambda expressions as last argument, cf. Swift.
+
    [[nodiscard]] inline std::vector< parse_error_base > flatten( const std::exception_ptr& ptr = std::current_exception() )
    {
       std::vector< parse_error_base > result;
       inspect< parse_error_base >( ptr, [ &result ]( const parse_error_base& e, const std::size_t /*unused*/ ) {
+         result.emplace_back( e );
+      } );
+      return result;
+   }
+
+   [[nodiscard]] inline std::vector< parse_error_base > flatten( const parse_error_base& exception )
+   {
+      std::vector< parse_error_base > result;
+      inspect< parse_error_base >( exception, [ &result ]( const parse_error_base& e, const std::size_t /*unused*/ ) {
          result.emplace_back( e );
       } );
       return result;
---
+++ src/test/pegtl/contrib_nested_exceptions.cpp
@@ -0,0 +1,208 @@
+// Copyright (c) 2024 Dr. Colin Hirsch and Daniel Frey
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined( __cpp_exceptions ) || !defined( __cpp_rtti )
+#include <iostream>
+int main()
+{
+   std::cout << "Exception and/or RTTI support disabled, skipping test..." << std::endl;
+}
+#else
+
+#include "test.hpp"
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <tao/pegtl/contrib/nested_exceptions.hpp>
+
+namespace TAO_PEGTL_NAMESPACE
+{
+   const std::string s1 = "foo";
+   const std::string s2 = "bar";
+
+   struct visitor
+   {
+      void operator()( const std::string& s, const std::size_t l )
+      {
+         v.emplace_back( std::string( "string%" ) + s, l );
+      }
+
+      void operator()( const std::exception& e, const std::size_t l )
+      {
+         v.emplace_back( std::string( "exception%" ) + e.what(), l );
+      }
+
+      void operator()( const std::runtime_error& e, const std::size_t l )
+      {
+         v.emplace_back( std::string( "runtime%" ) + e.what(), l );
+      }
+
+      std::vector< std::pair< std::string, std::size_t > > v;
+   };
+
+   void test1()
+   {
+      try {
+         throw s1;
+      }
+      catch( ... ) {
+         visitor v;
+         inspect< std::string >( v );
+         TAO_PEGTL_TEST_ASSERT( v.v.size() == 1 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].first == "string%foo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].second == 0 );
+      }
+   }
+
+   void test2()
+   {
+      try {
+         throw s1;
+      }
+      catch( const std::string& s ) {
+         visitor v;
+         inspect< std::string >( s, v );
+         TAO_PEGTL_TEST_ASSERT( v.v.size() == 1 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].first == "string%foo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].second == 0 );
+      }
+   }
+
+   void test3()
+   {
+      try {
+         try {
+            throw s1;
+         }
+         catch( ... ) {
+            std::throw_with_nested( std::runtime_error( s2 ) );
+         }
+      }
+      catch( ... ) {
+         visitor v;
+         inspect< std::string, std::runtime_error >( v );
+         TAO_PEGTL_TEST_ASSERT( v.v.size() == 2 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].first == "string%foo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].second == 1 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].first == "runtime%bar" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].second == 0 );
+      }
+   }
+
+   void test4()
+   {
+      try {
+         try {
+            throw std::logic_error( s1 );
+         }
+         catch( ... ) {
+            std::throw_with_nested( std::runtime_error( s2 ) );
+         }
+      }
+      catch( ... ) {
+         visitor v;
+         inspect< std::exception, std::runtime_error >( v );
+         TAO_PEGTL_TEST_ASSERT( v.v.size() == 2 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].first == "exception%foo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].second == 1 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].first == "runtime%bar" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].second == 0 );
+      }
+   }
+
+   void test5()
+   {
+      try {
+         try {
+            throw std::logic_error( s1 );
+         }
+         catch( ... ) {
+            std::throw_with_nested( std::runtime_error( s2 ) );
+         }
+      }
+      catch( ... ) {
+         visitor v;
+         inspect< std::runtime_error, std::exception >( v );  // Testing WRONG ORDER of exception classes!
+         TAO_PEGTL_TEST_ASSERT( v.v.size() == 2 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].first == "exception%foo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].second == 1 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].first == "exception%bar" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].second == 0 );
+      }
+   }
+
+   void test6()
+   {
+      try {
+         try {
+            try {
+               throw std::string( "yo" );
+            }
+            catch( ... ) {
+               std::throw_with_nested( std::logic_error( s1 ) );
+            }
+         }
+         catch( ... ) {
+            std::throw_with_nested( std::runtime_error( s2 ) );
+         }
+      }
+      catch( ... ) {
+         visitor v;
+         inspect< std::string, std::exception, std::runtime_error >( v );
+         TAO_PEGTL_TEST_ASSERT( v.v.size() == 3 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].first == "string%yo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].second == 2 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].first == "exception%foo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].second == 1 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 2 ].first == "runtime%bar" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 2 ].second == 0 );
+      }
+   }
+
+   void test7()
+   {
+      try {
+         try {
+            try {
+               throw std::string( "yo" );
+            }
+            catch( ... ) {
+               std::throw_with_nested( std::logic_error( s1 ) );
+            }
+         }
+         catch( ... ) {
+            std::throw_with_nested( std::runtime_error( s2 ) );
+         }
+      }
+      catch( const std::exception& e ) {
+         visitor v;
+         inspect< std::string, std::exception, std::runtime_error >( e, v );
+         TAO_PEGTL_TEST_ASSERT( v.v.size() == 3 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].first == "string%yo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 0 ].second == 2 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].first == "exception%foo" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 1 ].second == 1 );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 2 ].first == "exception%bar" );
+         TAO_PEGTL_TEST_ASSERT( v.v[ 2 ].second == 0 );
+      }
+   }
+
+   void unit_test()
+   {
+      test1();
+      test2();
+      test3();
+      test4();
+      test5();
+      test6();
+      test7();
+   }
+
+}  // namespace TAO_PEGTL_NAMESPACE
+
+#include "main.hpp"
+
+#endif
