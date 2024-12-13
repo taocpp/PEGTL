1efdaf14a4258ca8731d69144ec490a0667bd02f
Dr. Colin Hirsch <github@colin-hirsch.net>
2024-12-13 18:09:53 +0100

    Add missing keyword.

--- src/test/pegtl/buffer_text_buffer_input.cpp
+++ src/test/pegtl/buffer_text_buffer_input.cpp
@@ -14,7 +14,7 @@ namespace TAO_PEGTL_NAMESPACE
       in.require( 10 );
       const auto m = in.rewind_position();
       TAO_PEGTL_TEST_ASSERT( in.previous( m ) == in.current() );
-      in.consume< any >( 6 );
+      in.template consume< any >( 6 );
       TAO_PEGTL_TEST_ASSERT( in.previous( m ) + 6 == in.current() );
       in.rewind_to_position( m );
       TAO_PEGTL_TEST_ASSERT( in.previous( m ) == in.current() );
@@ -22,7 +22,7 @@ namespace TAO_PEGTL_NAMESPACE
       TAO_PEGTL_TEST_ASSERT( in.direct_count() == in.current_position().count );
       TAO_PEGTL_TEST_ASSERT( in.direct_line() == in.current_position().line );
       TAO_PEGTL_TEST_ASSERT( in.direct_column() == in.current_position().column );
-      in.consume< alpha >( 5 );
+      in.template consume< alpha >( 5 );
       const auto n = in.rewind_position();
       in.consume< eol >( 1 );
       TAO_PEGTL_TEST_ASSERT( in.direct_count() == 6 );
