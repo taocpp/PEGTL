8cd4700a75259c631cde76bea47ca40118878ee4
Dr. Colin Hirsch <github@colin-hirsch.net>
2024-12-13 18:15:11 +0100

    Add missing include.

--- src/test/pegtl/internal_constexpr.cpp
+++ src/test/pegtl/internal_constexpr.cpp
@@ -2,6 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 
+#include <functional>
 #include <type_traits>
 
 #include "test.hpp"
