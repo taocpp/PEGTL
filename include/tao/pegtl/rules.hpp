ac0f487c1eccf9a9451414fc3fea9c1c551d396c
Dr. Colin Hirsch <github@colin-hirsch.net>
2026-03-02 19:22:20 +0100

    Remove experimental combine and invert rules.

--- include/tao/pegtl/rules.hpp
+++ include/tao/pegtl/rules.hpp
@@ -15,8 +15,6 @@
 
 #include "config.hpp"
 
-#include "internal/combine_traits.hpp"
-#include "internal/invert_traits.hpp"
 #include "internal/rule_aliases.hpp"
 #include "internal/rules.hpp"
 
@@ -29,7 +27,6 @@ namespace TAO_PEGTL_NAMESPACE
    template< typename... Rules > struct at : internal::at< Rules... > {};
    struct bof : internal::bof {};
    struct bol : internal::bol {};
-   template< typename Left, typename Right > struct combine : internal::combine_traits< typename Left::rule_t, typename Right::rule_t >::rule_t {};
    template< std::size_t Count > struct consume : internal::consume< Count > {};
    template< template< typename... > class Control, typename... Rules > struct control : internal::control< Control, Rules... > {};
    template< typename... Rules > struct disable : internal::disable< Rules... > {};
@@ -42,7 +39,6 @@ namespace TAO_PEGTL_NAMESPACE
    template< auto Function, typename Peek = void > struct function : internal::function< Peek, decltype( Function ), Function > {};
    template< typename Rule, typename... Actions > struct if_apply : internal::if_apply< Rule, Actions... > {};
    template< typename Cond, typename Then, typename Else > struct if_then_else : internal::if_then_else< Cond, Then, Else > {};
-   template< typename Rule > struct invert : internal::invert_traits< typename Rule::rule_t >::rule_t {};
    template< typename Rule, typename Sep, typename Pad = void > struct list : internal::list< Rule, internal::pad< Sep, Pad > > {};
    template< typename Rule, typename Sep > struct list< Rule, Sep, void > : internal::list< Rule, Sep > {};
    template< typename Rule, typename Sep, typename Pad = void > struct list_tail : internal::list_tail_pad< Rule, Sep, Pad > {};
