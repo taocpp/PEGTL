51c074d0c2fa6ba53131b4fe4c60da4fb20a08c2
Dr. Colin Hirsch <github@colin-hirsch.net>
2026-03-07 21:44:17 +0100

    Remove support for empty ranges.

--- include/tao/pegtl/internal/ranges.hpp
+++ include/tao/pegtl/internal/ranges.hpp
@@ -10,7 +10,7 @@
 #include "../config.hpp"
 
 #include "enable_control.hpp"
-#include "failure.hpp"
+#include "dependent_false.hpp"
 #include "one.hpp"
 #include "range.hpp"
 #include "terminal.hpp"
@@ -22,22 +22,22 @@ namespace TAO_PEGTL_NAMESPACE::internal
       : terminal< ranges< Peek, Cs... > >
    {
       template< typename Data, Data Lo, Data Hi >
-      [[nodiscard]] static constexpr bool test_two( const Data c ) noexcept
+      [[nodiscard]] static constexpr bool test_impl( const Data c ) noexcept
       {
          static_assert( Lo <= Hi );
          return ( Lo <= c ) && ( c <= Hi );
       }
 
       template< typename Data, std::size_t... Is >
-      [[nodiscard]] static constexpr bool test_one( std::index_sequence< Is... > /*unused*/, const Data c ) noexcept
+      [[nodiscard]] static constexpr bool test_impl( std::index_sequence< Is... > /*unused*/, const Data c ) noexcept
       {
          constexpr Data cs[] = { Cs... };
 
          if constexpr( sizeof...( Cs ) % 2 == 0 ) {
-            return ( test_two< Data, cs[ 2 * Is ], cs[ 2 * Is + 1 ] >( c ) || ... );
+            return ( test_impl< Data, cs[ 2 * Is ], cs[ 2 * Is + 1 ] >( c ) || ... );
          }
          else {
-            return ( test_two< Data, cs[ 2 * Is ], cs[ 2 * Is + 1 ] >( c ) || ... ) || ( c == cs[ sizeof...( Cs ) - 1 ] );
+            return ( test_impl< Data, cs[ 2 * Is ], cs[ 2 * Is + 1 ] >( c ) || ... ) || ( c == cs[ sizeof...( Cs ) - 1 ] );
          }
       }
 
@@ -46,7 +46,7 @@ namespace TAO_PEGTL_NAMESPACE::internal
       {
          using data_t = typename Peek::data_t;
          static_assert( sizeof( Data ) <= sizeof( data_t ) );
-         return test_one( std::make_index_sequence< sizeof...( Cs ) / 2 >(), data_t( c ) );
+         return test_impl( std::make_index_sequence< sizeof...( Cs ) / 2 >(), data_t( c ) );
       }
    };
 
@@ -63,7 +63,9 @@ namespace TAO_PEGTL_NAMESPACE::internal
    template< typename Peek >
    struct ranges< Peek >
       : failure
-   {};
+   {
+      static_assert( dependent_false< Peek > );
+   };
 
    template< typename Peek, typename Peek::data_t... Cs >
    inline constexpr bool enable_control< ranges< Peek, Cs... > > = false;
