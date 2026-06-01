f742eb057c13d8b0e9447b2f87665effc512c00e
Dr. Colin Hirsch <github@colin-hirsch.net>
2026-06-01 21:15:12 +0200

    Improve record test.

--- src/test/extra_record.cpp
+++ src/test/extra_record.cpp
@@ -18,35 +18,65 @@ namespace TAO_PEGTL_NAMESPACE
    struct grammar : seq< sor_ab_ac, eof > {};
    // clang-format on
 
-   void test1()
+   template< typename Rule, typename Sample >
+   void verify_sample( const Sample& sample, const std::string_view data, const std::size_t line, const std::size_t column, const std::size_t count )
+   {
+      TAO_PEGTL_TEST_ASSERT( sample.rule == demangle< Rule >() );
+      TAO_PEGTL_TEST_ASSERT( sample.input.string_view() == data );
+
+      const auto& offset = sample.input.direct_offset();
+      TAO_PEGTL_TEST_ASSERT( offset.line == line );
+      TAO_PEGTL_TEST_ASSERT( offset.column == column );
+      TAO_PEGTL_TEST_ASSERT( offset.count == count );
+   }
+
+   void test_selected_terminals()
    {
       text_view_input<> in( "ac" );
       const auto v = record< one_a, one_b, one_c >::parse< grammar >( in );
       TAO_PEGTL_TEST_ASSERT( v.size() == 2 );
-      std::cout << v << std::endl;
+      verify_sample< one_a >( v[ 0 ], "a", 1, 1, 0 );
+      verify_sample< one_c >( v[ 1 ], "c", 1, 2, 1 );
+      TAO_PEGTL_TEST_ASSERT( in.empty() );
    }
 
-   void test2()
+   void test_first_branch()
+   {
+      text_view_input<> in( "ab" );
+      const auto v = record< one_a, one_b, one_c, seq_ab, seq_ac, sor_ab_ac >::parse< grammar >( in );
+      TAO_PEGTL_TEST_ASSERT( v.size() == 4 );
+      verify_sample< one_a >( v[ 0 ], "a", 1, 1, 0 );
+      verify_sample< one_b >( v[ 1 ], "b", 1, 2, 1 );
+      verify_sample< seq_ab >( v[ 2 ], "ab", 1, 1, 0 );
+      verify_sample< sor_ab_ac >( v[ 3 ], "ab", 1, 1, 0 );
+      TAO_PEGTL_TEST_ASSERT( in.empty() );
+   }
+
+   void test_second_branch()
    {
       text_view_input<> in( "ac" );
-      const auto v = record< one_a, one_b, one_c, seq_ab >::parse< grammar >( in );
-      TAO_PEGTL_TEST_ASSERT( v.size() == 2 );
-      std::cout << v << std::endl;
+      const auto v = record< one_a, one_b, one_c, seq_ab, seq_ac, sor_ab_ac >::parse< grammar >( in );
+      TAO_PEGTL_TEST_ASSERT( v.size() == 4 );
+      verify_sample< one_a >( v[ 0 ], "a", 1, 1, 0 );
+      verify_sample< one_c >( v[ 1 ], "c", 1, 2, 1 );
+      verify_sample< seq_ac >( v[ 2 ], "ac", 1, 1, 0 );
+      verify_sample< sor_ab_ac >( v[ 3 ], "ac", 1, 1, 0 );
+      TAO_PEGTL_TEST_ASSERT( in.empty() );
    }
 
-   void test3()
+   void test_failure()
    {
       text_view_input<> in( "ad" );
       const auto v = record< one_a, one_b, one_c, seq_ab >::parse< grammar >( in );
       TAO_PEGTL_TEST_ASSERT( v.empty() );
-      std::cout << v << std::endl;
    }
 
    void unit_test()
    {
-      test1();
-      test2();
-      test3();
+      test_selected_terminals();
+      test_first_branch();
+      test_second_branch();
+      test_failure();
    }
 
 }  // namespace TAO_PEGTL_NAMESPACE
